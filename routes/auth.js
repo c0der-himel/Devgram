c1a0d559b29aa65d053d5331f5d280acb0952269
himel <himel777hussain@gmail.com>
2021-02-18 11:52:43 +0600

    signin route done

--- routes/auth.js
+++ routes/auth.js
@@ -39,4 +39,32 @@ router.post('/signup', (req, res) => {
     });
 });
 
+router.post('/signin', (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(422).json({ error: 'please, fill all the fields' });
+  }
+  UserModel.findOne({ email })
+    .then((savedData) => {
+      if (!savedData) {
+        return res.status(422).json({ error: 'invalid email or password' });
+      }
+      bcrypt
+        .compare(password, savedData.password)
+        .then((doMatch) => {
+          if (doMatch) {
+            res.json({ message: 'successfully signed in' });
+          } else {
+            return res.status(422).json({ error: 'invalid email or password' });
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 module.exports = router;
