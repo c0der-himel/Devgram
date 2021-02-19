cd14fb3148bd992d70ebf17cc1ab0a7a2312a2c3
himel <himel777hussain@gmail.com>
2021-02-19 11:02:33 +0600

    home & profile page done

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -3,8 +3,51 @@ const Home = () => {
     <section className="Home">
       <div className="container">
         <div className="row">
-          <div className="col-sm">
-            <h1 className="test">Home</h1>
+          <div className="col-sm-6 offset-sm-3">
+            <div className="home shadow-lg rounded-3 py-4 px-5 mb-5">
+              <div className="home-img py-2">
+                <h4>Bill Gates</h4>
+                <img
+                  className="img-fluid rounded-3 shadow-lg"
+                  src="https://miro.medium.com/max/10000/1*HLGtY6O2vUHqIyEbWdmBgA.jpeg"
+                  alt="img"
+                />
+              </div>
+              <div className="home-content py-2">
+                <div className="post py-2">
+                  <i className="bi bi-heart-fill text-danger"></i>
+                  <h5>title</h5>
+                  <p>this is javascript</p>
+                  <input
+                    className="form-control"
+                    type="text"
+                    placeholder="add a comment"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="home shadow-lg rounded-3 py-4 px-5 mb-5">
+              <div className="home-img py-2">
+                <h4>Bill Gates</h4>
+                <img
+                  className="img-fluid rounded-3 shadow-lg"
+                  src="https://image.freepik.com/free-vector/programmer-coding-young-man-freelancer-working-program-code-with-laptop-geek-coding-software-vector-concept_53562-9214.jpg"
+                  alt="img"
+                />
+              </div>
+              <div className="home-content py-2">
+                <div className="post py-2">
+                  <i className="bi bi-heart-fill text-danger"></i>
+                  <h5>title</h5>
+                  <p>this is javascript</p>
+                  <input
+                    className="form-control"
+                    type="text"
+                    placeholder="add a comment"
+                  />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -3,8 +3,64 @@ const Profile = () => {
     <section className="Profile">
       <div className="container">
         <div className="row">
+          <div className="col-sm-6">
+            <div className="profile-img text-center mb-5 ms-5 ps-5">
+              <img
+                className="shadow-lg"
+                src="https://i.ytimg.com/vi/_poaE1WdG3k/maxresdefault.jpg"
+                alt="Bill Gates"
+              />
+            </div>
+          </div>
+          <div className="col-sm-6">
+            <div className="profile-content mt-5">
+              <h3>Bill Gates</h3>
+              <div className="details d-flex mt-4">
+                <p className="lead me-5">0 posts</p>
+                <p className="lead me-5">0 followers</p>
+                <p className="lead me-5">0 following</p>
+              </div>
+            </div>
+          </div>
           <div className="col-sm">
-            <h1 className="test">Profile</h1>
+            <div className="profile-gallery text-center">
+              <hr />
+              <img
+                className="shadow-lg m-3"
+                src="https://i.ytimg.com/vi/_poaE1WdG3k/maxresdefault.jpg"
+                alt="Bill Gates"
+              />
+              <img
+                className="shadow-lg m-3"
+                src="https://i.ytimg.com/vi/_poaE1WdG3k/maxresdefault.jpg"
+                alt="Bill Gates"
+              />
+              <img
+                className="shadow-lg m-3"
+                src="https://i.ytimg.com/vi/_poaE1WdG3k/maxresdefault.jpg"
+                alt="Bill Gates"
+              />
+              <img
+                className="shadow-lg m-3"
+                src="https://i.ytimg.com/vi/_poaE1WdG3k/maxresdefault.jpg"
+                alt="Bill Gates"
+              />
+              <img
+                className="shadow-lg m-3"
+                src="https://i.ytimg.com/vi/_poaE1WdG3k/maxresdefault.jpg"
+                alt="Bill Gates"
+              />
+              <img
+                className="shadow-lg m-3"
+                src="https://i.ytimg.com/vi/_poaE1WdG3k/maxresdefault.jpg"
+                alt="Bill Gates"
+              />
+              <img
+                className="shadow-lg m-3"
+                src="https://i.ytimg.com/vi/_poaE1WdG3k/maxresdefault.jpg"
+                alt="Bill Gates"
+              />
+            </div>
           </div>
         </div>
       </div>
